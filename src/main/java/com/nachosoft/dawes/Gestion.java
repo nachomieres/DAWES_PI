9321331d3b58b5d6973ba0c665933d04477cbea1
i.gonzalez <i.gonzalez@RS-MTO-055.rioglassolar.com>
2019-10-10 15:21:01 +0200

    Version entregada

--- src/main/java/com/nachosoft/dawes/Gestion.java
+++ src/main/java/com/nachosoft/dawes/Gestion.java
@@ -5,29 +5,36 @@ import java.time.Period;
 import java.util.TreeMap;
 
 public class Gestion {
+	// Informacion de profesores, campo clave: dni
 	private TreeMap<String, Profesor> profesores;
+	// Informacion de Ciclos Formativos, campo clave: denCorta
 	private TreeMap<String, CicloFormativo> ciclos;
-	
-	public Gestion () {
+
+	// Constructor basico que instancia las colecciones
+	public Gestion() {
+		super();
 		this.profesores = new TreeMap<String, Profesor>();
 		this.ciclos = new TreeMap<String, CicloFormativo>();
 	}
-	
-	public void addProfesor (Profesor p) {
+
+	// Añade un profesor ya creado
+	public void addProfesor(Profesor p) {
 		this.profesores.put(p.getDni(), p);
 	}
-	
-	public void addCicloFormativo (CicloFormativo c) {
+
+	// Añade un ciclo formativo ya creado
+	public void addCicloFormativo(CicloFormativo c) {
 		this.ciclos.put(c.getDenCorta(), c);
 	}
-	
-	public void asignaProfesor (Profesor p, CicloFormativo c, LocalDate fecha) {
+
+	public void asignaProfesor(Profesor p, CicloFormativo c, LocalDate fecha) {
 		CicloImpartido cp = new CicloImpartido(p, c, fecha);
 		p.getCiclos().add(cp);
 		c.getProfesores().add(cp);
 	}
-	
-	public void getCiclosImpartidos (Profesor p) {
+
+	// Imprime los Ciclos Formativos impartidos de un profesor dado
+	public void getCiclosImpartidos(Profesor p) {
 		System.out.println("Ciclos del profesor " + p.getNombre());
 		profesores.get(p.getDni()).getCiclos().forEach(x -> {
 			System.out.println("- " + x.getCiclo().getDenCorta() + " - " + x.getCiclo().getDenLarga());
@@ -35,31 +42,35 @@ public class Gestion {
 		System.out.println("-----------------------------------------------------------");
 
 	}
-	
-	public void getProfesoresCiclo (CicloFormativo c) {
+
+	// Imprime los Profesores de un Ciclo Formativo dado
+	public void getProfesoresCiclo(CicloFormativo c) {
 		System.out.println("Profesores del Ciclo " + c.getDenCorta() + " - " + c.getDenLarga());
 		ciclos.get(c.getDenCorta()).getProfesores().forEach(x -> {
-			System.out.println("- " + x.getProfesor().getNombre() + " DNI: " + x.getProfesor().getDni() + " Imparte desde " + x.getFechaComienzo());
+			System.out.println("- " + x.getProfesor().getNombre() + " DNI: " + x.getProfesor().getDni()
+					+ " Imparte desde " + x.getFechaComienzo());
 		});
 		System.out.println("-----------------------------------------------------------");
 	}
-	
-	public void getCiclosyProfesores () {
+
+	// Imprime todos los Ciclos Formativos indicando los Profesores que los imparten
+	public void getCiclosyProfesores() {
 		this.ciclos.values().forEach(x -> {
 			System.out.println("Ciclo Formativo " + x.getDenCorta() + " - " + x.getDenLarga());
 			this.getProfesoresCiclo(x);
 		});
 		System.out.println("-----------------------------------------------------------");
 	}
-	
-	public void getProfesoresCicloMasTresAnnos (CicloFormativo c) {
+
+	// Imprime los Profesores que llevan mas de 3 años impartiendo el Ciclo Formativo indicado
+	public void getProfesoresCicloMasTresAnnos(CicloFormativo c) {
 		LocalDate hoy = LocalDate.now();
-		this.ciclos.get(c.getDenCorta()).getProfesores().forEach(x -> {			
+		this.ciclos.get(c.getDenCorta()).getProfesores().forEach(x -> {
 			if (Period.between(x.getFechaComienzo(), hoy).getYears() >= 3) {
-				System.out.println("- " + x.getProfesor().getNombre() + " DNI: " + x.getProfesor().getDni() + " Imparte desde " + x.getFechaComienzo());
+				System.out.println("- " + x.getProfesor().getNombre() + " DNI: " + x.getProfesor().getDni()
+						+ " Imparte desde " + x.getFechaComienzo());
 			}
 		});
 	}
-	
 
 }
